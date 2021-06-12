72ca91049590ec2b50d8774dec887ca4a9396527
SajalVaishnav <76965917+SajalVaishnav@users.noreply.github.com>
2021-06-12 12:05:19 +0800

    obligatory commit, after adding authentication.js, before pushing to heroku server

---
+++ server/routes/authentication.js
@@ -0,0 +1,94 @@
+//-----------------------------------------------IMPORTING STUFF
+const express = require('express');
+const router = express.Router();
+
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const passportConfig = require('../config/passportConfig');
+
+//-----------------------------------------------MIDDLEWARE FOR SESSIONS AND AUTHENTICATION
+router.use(session ({
+  secret: 'awonderfulworld',
+  resave: false,
+  saveUnitialized: false
+}));
+router.use(cookieParser('awonderfulworld'));
+router.use(passport.initialize());
+router.use(passport.session());
+passportConfig(passport);
+
+
+//-----------------------------------------------SETTING UP ROUTES
+router.post("/login", (req, res, next) => {
+  const userIdWithNusExtension = req.body.username + '@u.nus.edu';
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send({
+      loginAttempt: false,
+    });
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send({
+          loginAttempt: true,
+          userId: req.user.username
+        });
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+router.post("/register", (req, res) => {
+  const userIdWithNusExtension = req.body.username + '@u.nus.edu';
+  User.findOne({ username: userIdWithNusExtension }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) 
+      res.send({
+        signupAttempt: false,
+        userId: userIdWithNusExtension
+      });
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      const newUser = new User({
+        username: userIdWithNusExtension,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send({
+        signupAttempt: true,
+        userId: userIdWithNusExtension
+      });
+    }
+  });
+});
+
+router.get("/user", (req, res) => { 
+  
+  if (req.isAuthenticated()) {
+    console.log('authentic request');
+    res.send({
+      username: req.user.username,
+      loggedin: true
+    }); // The req.user stores the entire user that has been authenticated inside of it.
+  } else {  
+    console.log('inauthentic request!!')
+    res.send({
+      username: null,
+      loggedin: true
+    });
+  } 
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.send(req.body.username + 'logged out');
+});
+
+
+
+module.exports = router;
