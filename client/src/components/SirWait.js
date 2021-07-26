053c0e6aadd47f6654d80b24847314b75d3d807f
MontyPython28 <samyusubs@gmail.com>
2021-07-26 11:42:34 +0800

    Changed text from white to blue

--- client/src/components/SirWait.js
+++ client/src/components/SirWait.js
@@ -5,8 +5,8 @@ const SirWait = () => {
       <div>
         <div className="columns is-vcentered">
           <div className="column has-text-centered">
-          <h1 className="subtitle is-3 has-text-white">We've sent an email to your Outlook with a link. Please click on it to register.</h1>
-          {/* <h1 className="title is-3 has-text-white">Don't forget to check your spam!</h1> */}
+          <h1 className="subtitle is-3 has-text-link">We've sent an email to your Outlook with a link. Please click on it to register.</h1>
+         <h1 className="title is-3 has-text-link">Don't forget to check your spam!</h1>
           </div>
         </div>
       </div>
